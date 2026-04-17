2575ebc6b79b8f92419c8cc8ab2db6e3926a9270
Bangdayeon <dybang00@gmail.com>
2026-04-18 07:09:06 +0900

    add google html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "빙킷 | 빙고로 이루는 투두 & 버킷리스트 앱",
   description:
     "빙고판으로 목표를 관리하는 새로운 투두 앱, 빙킷. 버킷리스트를 더 재미있게 달성하고 친구와 함께 도전해보세요.",
-  
+
   keywords: [
     "빙고 투두",
     "버킷리스트 앱",
@@ -19,18 +19,18 @@ export const metadata: Metadata = {
     "모바일 앱",
     "iOS 앱",
     "커뮤니티",
-    "라이프스타일"
+    "라이프스타일",
   ],
 
   openGraph: {
     title: "빙킷 | 빙고로 이루는 투두 & 버킷리스트",
     description:
       "목표를 빙고판으로 만들고, 채워가며 달성하는 새로운 방식. 지금 바로 체험해보세요.",
-    url: "https://너네도메인.com",
+    url: "https://your-domain.com",
     siteName: "빙킷",
     images: [
       {
-        url: "/images/og.png", // 반드시 따로 제작 추천
+        url: "/images/og.png",
         width: 1200,
         height: 630,
         alt: "빙킷 앱 미리보기",
@@ -54,7 +54,11 @@ export const metadata: Metadata = {
   },
 
   alternates: {
-    canonical: "https://너네도메인.com",
+    canonical: "https://your-domain.com",
+  },
+
+  verification: {
+    google: "arSWwXNjwRh-goFhXrzfFCT2kVnh0O-sJw6BC-ZBIVw",
   },
 };
 
