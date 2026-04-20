5afff67abf904ccc52b17a52ec9f95fce4004b9a
Bangdayeon <dybang00@gmail.com>
2026-04-20 16:49:41 +0900

    add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/Header";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "빙킷",
@@ -14,6 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-C1VD72NR9M" strategy="afterInteractive" />
+      <Script id="gtag-init" strategy="afterInteractive">{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-C1VD72NR9M');
+      `}</Script>
       <body className="min-h-screen flex flex-col items-center bg-linear-to-tr from-[#E8FAFE] to-[#F2FDE8] custom-scrollbar">
         <Header />
         <main className="flex flex-1 w-full max-w-6xl">{children}</main>
